453cdca48e4fcef3891d18d1c623f3dc1ea4e287
Success <121008519+Success05081988@users.noreply.github.com>
2024-01-31 21:56:49 +0500

    Add files via upload

---
+++ packages-2024.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+
+pacman -S \
+        grub \
+        efibootmgr \
+        networkmanager \
+        network-manager-applet \
+        dialog \
+        wpa_supplicant \
+        mtools \
+        dosfstools \
+        reflector \
+        base-devel \
+        linux-headers \
+        linux-lts-headers \
+        bluez \
+        bluez-utils \
+        cups \
+        hplip \
+        alsa-utils \
+        pipewire \
+        pipewire-alsa \
+        pipewire-pulse \
+        pipewire-jack \
+        bash-completion \
+        openssh \
+        rsync \
+        acpi \
+        acpi_call \
+        acpi_call-lts \
+        tlp \
+        sof-firmware \
+        acpid \
+        os-prober \
+        ntfs-3g \
+        fwupd \
+        git \
