d4cf6210636fe17a749eca95e336828327efd93b
Success <121008519+Success05081988@users.noreply.github.com>
2024-02-09 02:27:52 +0500

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo "<<------ START INSTALL ARCH-LINUX ------>>"
+sleep 5
 pacman -Syy
 ln -sf /usr/share/zoneinfo/Asia/Tashkent /etc/localtime
 hwclock --systohc
@@ -16,29 +17,27 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 sleep 5
 echo root:password | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-zen-headers alsa-utils jack2 bash-completion openssh rsync acpi sof-firmware acpid ntfs-3g terminus-font ttf-terminus-nerd ttf-roboto man fwupd
+pacman -S --noconfirm base-devel linux-lts linux-lts-headers sof-firmware grub alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector bluez bluez-utils cups hplip bash-completion openssh rsync acpi acpi_call-lts tlp acpid ntfs-3g terminus-font ttf-terminus-nerd ttf-roboto man fwupd git
+
+sleep 5
+
+pacman -S --noconfirm xorg grub-btrfs
+
+sleep 5
+
+pacman -S --noconfirm plasma 
 
 sleep 10
 
-pacman -S --noconfirm grub-btrfs
+pacman -S --noconfirm egl-wayland konsole dolphin plasma-wayland-session neofetch htop inxi bpytop elisa vlc chromium okular gwenview obs-studio
 
-sleep 10
-
-pacman -S --noconfirm xorg
-
-sleep 10
-
-pacman -S --noconfirm plasma egl-wayland konsole dolphin
-#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
-sleep 10
+sleep 5
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-#grub-install --target=i386-pc /dev/sda
 #grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sleep 10
+sleep 5
 
 systemctl enable NetworkManager
 #systemctl enable bluetooth
@@ -52,14 +51,11 @@ systemctl enable acpid
 
 sleep 5
 
-useradd -m orif1988
+useradd -m test
 echo test:password | chpasswd
-echo "orif1988 ALL=(ALL) ALL" >> /etc/sudoers.d/orif1988
-usermod -c 'Orif Ismailov' orif1988
+echo "test ALL=(ALL) ALL" >> /etc/sudoers.d/test
+usermod -c 'Orif Ismailov' test
 
-sleep 10
+sleep 5
 
 printf "\e[1;32mDone! Type exit, umount -a /mnt and reboot.\e[0m"
-
-
-
