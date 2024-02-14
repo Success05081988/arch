f0ee82bf97805679bdce5872b0c107ec1d3e1320
Success <121008519+Success05081988@users.noreply.github.com>
2024-02-14 12:49:40 +0500

    Update base-mbr.sh

--- base-mbr.sh
+++ base-mbr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "<<------ START INSTALL ARCH-LINUX ------>>"
-sleep 5
+sleep 3
 pacman -Syy
 ln -sf /usr/share/zoneinfo/Asia/Tashkent /etc/localtime
 hwclock --systohc
 sed -i '398s/.//' /etc/locale.gen
 locale-gen
-sleep 5
+sleep 3
 echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=ru" >> /etc/vconsole.conf
 echo "FONT=cyr-sun16" >> /etc/vconsole.conf
@@ -14,22 +14,22 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-sleep 5
+sleep 3
 echo root:password | chpasswd
 
-pacman -S --noconfirm base-devel linux-lts linux-lts-headers sof-firmware grub alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector bluez bluez-utils cups hplip bash-completion openssh rsync acpi acpi_call-lts tlp acpid ntfs-3g terminus-font ttf-terminus-nerd ttf-roboto man fwupd git
+pacman -S --noconfirm linux-lts linux-lts-headers sof-firmware grub alsa-utils pipewire pipewire-pulse pipewire-jack efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector bluez bluez-utils cups hplip bash-completion openssh rsync acpi acpi_call-lts tlp acpid ntfs-3g terminus-font ttf-terminus-nerd ttf-roboto man fwupd git os-prober
 
-sleep 5
+sleep 3
 
 pacman -S --noconfirm xorg grub-btrfs
 
-sleep 5
+sleep 3
 
 pacman -S --noconfirm plasma 
 
-sleep 10
+sleep 5
 
-pacman -S --noconfirm egl-wayland konsole dolphin plasma-wayland-session neofetch htop inxi bpytop elisa vlc chromium okular gwenview obs-studio
+pacman -S --noconfirm egl-wayland konsole dolphin plasma-wayland-session neofetch htop inxi bpytop elisa vlc chromium okular gwenview ark
 
 sleep 5
 
@@ -37,7 +37,7 @@ grub-install --target=i386-pc /dev/sdb
 #grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sleep 5
+sleep 3
 
 systemctl enable NetworkManager
 #systemctl enable bluetooth
@@ -49,14 +49,14 @@ systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
-sleep 5
+sleep 3
 
-useradd -m test
-echo test:password | chpasswd
-echo "test ALL=(ALL) ALL" >> /etc/sudoers.d/test
-usermod -c 'Orif Ismailov' test
+useradd -m orif1988
+echo orif1988:password | chpasswd
+echo "orif1988 ALL=(ALL) ALL" >> /etc/sudoers.d/orif1988
+usermod -c 'Orif Ismailov' orif1988
 
-sleep 5
+sleep 3
 
 printf "\e[1;32mDone! Type exit, umount -a /mnt and reboot.\e[0m"
 
