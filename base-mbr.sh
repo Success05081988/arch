3db52ff94cdea10682a399a067344a9bdd643bf8
Success <121008519+Success05081988@users.noreply.github.com>
2024-02-24 21:10:41 +0500

    Update base-mbr.sh

--- base-mbr.sh
+++ base-mbr.sh
@@ -17,11 +17,16 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 sleep 3
 echo root:password | chpasswd
 
-pacman -S --noconfirm linux-lts-headers sof-firmware grub alsa-utils pipewire pipewire-pulse pipewire-jack efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector bluez bluez-utils cups hplip bash-completion openssh rsync acpi acpi_call-lts tlp acpid ntfs-3g terminus-font ttf-terminus-nerd ttf-roboto man fwupd git os-prober
+pacman -S --noconfirm linux-lts-headers sof-firmware grub alsa-utils \
+efibootmgr networkmanager network-manager-applet dialog \
+wpa_supplicant mtools dosfstools reflector bash-completion \
+openssh rsync acpi acpi_call-lts tlp acpid ntfs-3g \
+terminus-font ttf-terminus-nerd ttf-roboto man fwupd git os-prober
+#for printers and bluetoth (bluez bluez-utils cups hplip)
 
 sleep 3
 
-pacman -S --noconfirm xorg grub-btrfs
+pacman -S --noconfirm xorg
 
 sleep 3
 
@@ -29,11 +34,14 @@ pacman -S --noconfirm plasma
 
 sleep 5
 
-pacman -S --noconfirm egl-wayland konsole dolphin plasma-wayland-session neofetch htop inxi bpytop elisa vlc chromium okular gwenview ark
+pacman -S --noconfirm egl-wayland konsole dolphin plasma-wayland-session \
+neofetch htop inxi bpytop elisa vlc chromium \
+okular gwenview ark vim vim-plugins partitionmanager \
+spectacle telegram-desktop clipgrab 
 
 sleep 5
 
-grub-install --target=i386-pc /dev/sdc
+grub-install --target=i386-pc /dev/sda
 #grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
 grub-mkconfig -o /boot/grub/grub.cfg
 
